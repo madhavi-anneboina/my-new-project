b87558a922231c89fca1e5e782629fcc8a4cd2ae
madhavi-anneboina <128103581+madhavi-anneboina@users.noreply.github.com>
2023-03-17 19:26:37 -0400

    raect native state

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
+  const [name, setName] = useState('madhavi')
+  const clickHandler = e =>{
+    setName('ramani')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>my name is {name}</Text>
       <StatusBar style="auto" />
+      <Button onclick={clickHandler}> clike me </Button>
     </View>
   );
 }
